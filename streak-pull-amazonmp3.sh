bf024e34bd1c4b04e120fd8b844376dd023a2dbb
unknown <Tom@.exony.local>
2010-08-26 09:45:36 +0100

    added a central settings file

--- streak-pull-amazonmp3.sh
+++ streak-pull-amazonmp3.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
+. ./settings
 
 # check that the streak is mounted
-if [ ! -d "/media/STREAK" ]; then
+if [ ! -d "$REMOTE_PATH" ]; then
 	echo "ERR: You must first mount the streak"
 	exit 1
 fi
 
 echo "pulling purchases from streak to computer..."
-rsync -ruv /media/STREAK/amazonmp3/ ~/Music/Collection/Amazon\ MP3/
-rm -rf /media/STREAK/amazonmp3/*
+rsync -ruv $REMOTE_PATH/amazonmp3/ $LOCAL_PATH/Amazon\ MP3/
+rm -rf $REMOTE_PATH/amazonmp3/*
 echo "done"
--- streak-push-music.sh
+++ streak-push-music.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+. ./settings
 
 # check that the streak is mounted
-if [ ! -d "/media/STREAK" ]; then
+if [ ! -d "$REMOTE_PATH" ]; then
 	echo "ERR: You must first mount the streak"
 	exit 1
 fi
@@ -9,9 +10,9 @@ fi
 echo "syncing music to streak.."
 
 echo "--Collection--"
-rsync -ruv ~/Music/Collection/ /media/STREAK/Music/
+rsync -ruv $LOCAL_PATH/ $REMOTE_PATH/Music/
 
 echo "--UbuntuOne--"
-rsync -ruv ~/.local/share/ubuntuone/Purchased\ from\ Ubuntu\ One/ /media/STREAK/Music/
+rsync -ruv "~/.local/share/ubuntuone/Purchased from Ubuntu One/" "$REMOTE_PATH/Music/"
 
 echo "done"
