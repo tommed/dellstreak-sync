70d286ae75dd06effd954aecd2a9171786f00768
Tom Medhurst <tom.medhurst@gmail.com>
2010-08-26 09:59:47 +0100

    added switches for ubuntu one purchases and rsync switches

--- streak-pull-amazonmp3.sh
+++ streak-pull-amazonmp3.sh
@@ -8,6 +8,10 @@ if [ ! -d "$REMOTE_PATH" ]; then
 fi
 
 echo "pulling purchases from streak to computer..."
-rsync -ruv $REMOTE_PATH/amazonmp3/ $LOCAL_PATH/Amazon\ MP3/
-rm -rf $REMOTE_PATH/amazonmp3/*
+rsync $RSYNC_ARGS "$REMOTE_PATH/amazonmp3/" "$LOCAL_PATH/Amazon\ MP3/"
+
+if [ "$CLEAR_AMAZONMP3" = "yes" ]; then
+	rm -rf $REMOTE_PATH/amazonmp3/*
+fi
+
 echo "done"
--- streak-push-music.sh
+++ streak-push-music.sh
@@ -9,10 +9,16 @@ fi
 
 echo "syncing music to streak.."
 
+# main music collection
 echo "--Collection--"
-rsync -ruv $LOCAL_PATH/ $REMOTE_PATH/Music/
+rsync $RSYNC_ARGS "$LOCAL_PATH/" "$REMOTE_PATH/Music/"
 
-echo "--UbuntuOne--"
-rsync -ruv "~/.local/share/ubuntuone/Purchased from Ubuntu One/" "$REMOTE_PATH/Music/"
+# ubuntu one collection
+if [ "$INCLUDE_UBUNTU_ONE" = "yes" ]; then
+	echo "--UbuntuOne--"
+	rsync $RSYNC_ARGS "~/.local/share/ubuntuone/Purchased from Ubuntu One/" "$REMOTE_PATH/Music/"
+else
+	echo "WARN: Skipping Ubuntu One purchases"
+fi
 
 echo "done"
